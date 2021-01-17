99203b694cef562c4c514318af863c84e332565b
seb776 <sebastien.maire@epitech.eu>
2021-01-17 19:26:17 +0100

    Simple shader generation and save

---
+++ AutoShader/Assets/HandleShaderGen.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class ColorPalette
+{
+    public Color Back;
+    public Color[] Shapes;
+}
+
+public class HandleShaderGen : MonoBehaviour
+{
+    public ColorPalette[] Palettes;
+    public RenderTexture RenderTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _templateCode = File.ReadAllText("Assets/TemplateShader.shader");
+    }
+
+    string _templateCode;
+
+    void GenerateShaderCode(string outputPath, string shaderName)
+    {
+        StringBuilder sb = new StringBuilder();
+        var palette = Palettes[UnityEngine.Random.Range(0, Palettes.Length)];
+
+
+        sb.AppendLine("float shp = 400.;");
+        sb.AppendLine($"col = float3({palette.Back.r}, {palette.Back.g}, {palette.Back.b});");
+        sb.AppendLine("float3 rgb;");
+        sb.AppendLine("float shape;");
+        sb.AppendLine($"float rep = {UnityEngine.Random.Range(0.01f, 0.07f)};");
+
+        if (UnityEngine.Random.Range(0.0f,1.0f) < 0.5f)
+            sb.AppendLine($"float border = _sqr(mul(fmod(uv+rep*0.5, float2(rep, rep))-rep*0.5, r2d({UnityEngine.Random.Range(-5.0f, 5.0f)})), float2({UnityEngine.Random.Range(1.0f, 2.0f)}, rep*{UnityEngine.Random.Range(0.15f, 0.4f)}));");
+        else
+            sb.AppendLine($"float border = _sqr(mul(uv, r2d({UnityEngine.Random.Range(-5.0f, 5.0f)})), float2({UnityEngine.Random.Range(1.0f,2.0f)}, {UnityEngine.Random.Range(0.15f,0.4f)}));");
+
+        for (int i = 0; i < 15; ++i)
+        {
+            var colShape = palette.Shapes[UnityEngine.Random.Range(0, palette.Shapes.Length)];
+            sb.AppendLine($"rgb = float3({colShape.r}, {colShape.g}, {colShape.b});");
+
+            string shaderFunc = (UnityEngine.Random.Range(0.0f, 1.0f) > 0.5f ? "_cir" : "_loz");
+            string pos = $"float2({UnityEngine.Random.Range(-1.0f, 1.0f)}, {UnityEngine.Random.Range(-1.0f, 1.0f)})";
+            sb.AppendLine($"shape = {shaderFunc}(uv+{pos}, {UnityEngine.Random.Range(0.1f, 0.5f)});");
+            sb.AppendLine($"shape = max(shape, border);");
+            sb.AppendLine("col = lerp(col, rgb, 1.-sat(shape*shp));");
+        }
+
+        string[] colMap = {
+            "col = col.zyx;",
+            "col = col.zxy;",
+            "col = col.yzx;",
+        };
+
+        if (UnityEngine.Random.Range(0.0f,1.0f) < 0.5f)
+        {
+            sb.AppendLine("col = 1.0-col;");
+        }
+
+
+        var newText = _templateCode.Replace("{0}", shaderName).Replace("{1}", sb.ToString());
+        //var shaderCode = string.Format(_templateCode, shaderName, addedCode);
+        File.WriteAllText(outputPath, newText);
+    }
+
+    private void Render(string name)
+    {
+        var shaderName = name;
+        var assetShaderPath = $"Assets/NewShaders/{shaderName}.shader";
+        GenerateShaderCode(assetShaderPath, shaderName);
+        UnityEditor.AssetDatabase.ImportAsset(assetShaderPath);
+        var shader = Shader.Find($"Unlit/{shaderName}");
+        var material = new Material(shader);
+        material.SetVector("iSize", new Vector2(RenderTexture.width, RenderTexture.height));
+        Graphics.Blit(null, RenderTexture, material);
+
+        SaveTexture($@"E:\OneDrive\Projects\Perso\Shaders\Records\Botz0rg_17_01_2021\{shaderName}.png", RenderTexture);
+
+        DestroyImmediate(material);
+        DestroyImmediate(shader);
+    }
+
+    public void SaveTexture(string path, RenderTexture rt)
+    {
+        var tex = toTexture2D(rt);
+        byte[] bytes = tex.EncodeToPNG();
+
+        System.IO.File.WriteAllBytes(path, bytes);
+
+        DestroyImmediate(tex);
+    }
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+
+    public bool DoRender;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (DoRender)
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                Render($"shader{i}");
+            }
+            DoRender = false;
+        }
+    }
+}
---
+++ AutoShader/Assets/TemplateShader.shader
@@ -0,0 +1,119 @@
+﻿Shader "Unlit/{0}"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+			float2 iSize;
+
+			float2x2 r2d(float a) { float c = cos(a), s = sin(a);  return float2x2(c,-s,s,c); }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+#define sat(a) clamp(a,0.,1.)
+			float lenny(float2 v)
+			{
+				return abs(v.x) + abs(v.y);
+			}
+
+			float _cir(float2 p, float r)
+			{
+				return length(p) - r;
+			}
+			float _loz(float2 p, float r)
+			{
+				return lenny(p) - r;
+			}
+			float _cirloz(float2 p, float r, float f)
+			{
+				return lerp(length(p), lenny(p), f) - r;
+			}
+
+			float _sqr(float2 p, float2 s)
+			{
+				float2 q = abs(p) - s;
+				return max(q.x, q.y);
+			}
+
+			float3 rdr(float2 uv)
+			{
+				float3 col;
+				
+				{1}
+
+				return col;
+			}
+
+#define AA 6
+			float3 rdrAA(float2 uv, float2 fragCoord)
+			{
+				float3 tot = float3(0.0, 0., 0.);
+#if AA>1
+				for (int m = 0; m < AA; m++)
+					for (int n = 0; n < AA; n++)
+					{
+						// pixel coordinates
+						float2 o = float2(float(m), float(n)) / float(AA) - 0.5;
+						float2 p = (2.0*(fragCoord + o) - iSize.xy) / iSize.y;
+#else    
+				float2 p = (2.0*fragCoord - iSize.xy) / iSize.y;
+#endif
+				float3 col = rdr(p);
+				tot += col;
+#if AA>1
+					}
+			tot /= float(AA*AA);
+#endif
+			return tot;
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float3 col;
+				col = rdrAA(i.uv, i.uv*iSize);
+				return float4(col.x, col.y, col.z, 1.0);
+
+				
+
+            }
+            ENDCG
+        }
+    }
+}
